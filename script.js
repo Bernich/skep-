a523870996c9124c6a5f72092c3e10fb4917ad36
Bernich <berni24695@gmail.com>
2020-08-05 05:51:07 +0000

    my first upload

---
+++ script.js
@@ -0,0 +1,16 @@
+alert("The power of code HBD!!!🎶🎵");
+window.addEventListener('click', function() {
+    var audio = document.getElementById("play");
+    audio.play();
+
+});
+
+
+//window.onclick = function() {
+//  var aud = document.getElementById("play");
+//   aud.volume = .8;
+//  aud.play();
+//
+//  var aud = document.getElementById("play");
+//   aud.volume = .14;
+//}
